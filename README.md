00be24be810ec57842a7ebdee68220d87fe51857
l3ol3r <bobruser2007@gmail.com>
2025-02-11 10:10:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mangaRenamer
+ Rename manga images from Mangalib for use it in Digital Comics on Psp
---
+++ main.py
@@ -0,0 +1,33 @@
+import os, shutil
+os.chdir('Manga')
+print('Analysing files...')
+filesCount = 0
+for i in os.listdir():
+    filesCount += len(os.listdir(os.getcwd() + '\\' + i))
+
+curr_page = 1
+folders = sorted(os.listdir(), key=lambda x:(float(x.split('-')[0][1:]), float(x.split('-')[1][1:])))
+
+page_rank = len(str(filesCount))
+volume_rank = 3
+chapter_rank = 4
+
+print('Files count:', filesCount)
+print('Start renaming...')
+for i in folders:
+    os.chdir(i)
+    files = sorted(os.listdir(), key = lambda x: int(x.split('-')[0][1:]))
+    for name in files:
+        path_dir = i.split('-')
+        new_name = 'v' + '0' * (volume_rank - len(path_dir[0]) + 1) + path_dir[0][1:] + '-n' + '0' * (chapter_rank - len(str(int(float(path_dir[1][1:]))))) + str(int(float(path_dir[1][1:]))) + '-p' + '0' * (page_rank - len(str(curr_page))) + str(curr_page) + '.jpg'
+        # new_name = 'p' + '0' * (page_rank - len(str(curr_page))) + str(curr_page) + '.jpg'
+        os.rename(name, '../../Export/' + new_name)
+        curr_page += 1
+    os.chdir('..')
+print('Complete!')
+
+os.chdir('..')
+
+print('Start archivation..')
+shutil.make_archive('Manga', 'zip', 'Export/')
+print('Complete!')
